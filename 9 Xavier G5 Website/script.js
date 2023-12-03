478e765bdd02ab96b724e98b83cf61efe6604532
unknown <gyuelnikolai07@gmail.com>
2023-12-03 13:45:07 +0800

    Initial commit

---
+++ 9 Xavier G5 Website/script.js
@@ -0,0 +1,7 @@
+import anime from 'JS Dependencies/anime.es.js';
+anime({
+    targets: '.big',
+    translateX: 250, // Move the element 250 pixels to the right
+    rotate: '1turn', // Rotate the element 360 degrees
+    duration: 2000 // Animation duration in milliseconds
+  });
